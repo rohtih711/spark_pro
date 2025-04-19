010c8588ea74dd8c63e6084f5ee72c62fbe1eab7
Rohith B <rohithb@Rohiths-Mac-mini.local>
2025-04-19 22:43:09 +0530

    new proble

--- medium.py
+++ medium.py
@@ -5,31 +5,77 @@ from pyspark.sql.window import Window
 
 spark = SparkSession.builder.appName("PeopleData").getOrCreate()
 
+data = [
+    (1, 2, "2016/06/03"),
+    (1, 3, "2016/06/08"),
+    (2, 3, "2016/06/08"),
+    (3, 4, "2016/06/09")
+]
+
 
 schema = StructType([
-    StructField("person_id", IntegerType(), True),
-    StructField("person_name", StringType(), True),
-    StructField("weight", IntegerType(), True),
-    StructField("turn", IntegerType(), True)
+    StructField("requester_id", IntegerType(), True),
+    StructField("accepter_id", IntegerType(), True),
+    StructField("accept_date", StringType(), True)
 ])
-
-data = [
-    (5, "Alice", 250, 1),
-    (4, "Bob", 175, 5),
-    (3, "Alex", 350, 2),
-    (6, "John Cena", 400, 3),
-    (1, "Winston", 500, 6),
-    (2, "Marie", 200, 4)
-]
 
 
 df = spark.createDataFrame(data, schema)
 
 
-wind  = Window.orderBy(col('turn').asc())
+col_d1 = df.selectExpr('requester_id as values')
+col_d2 = df.selectExpr('accepter_id as values')
 
-df1 = df.withColumn('sum',sum('weight').over(wind))
+col_3 = col_d1.union(col_d2)
 
-df2 = df1.filter(col('sum') == 1000).select('person_name')
+res1 = col_3.groupBy('values').agg(count('values').alias('count'))
 
-df2.show()
+win  =Window.orderBy(col('count').desc())
+
+res2 = res1.withColumn('rank',rank().over(win))
+
+res3 = res2.filter(col('rank') == 1).select(col('values'),col('count').alias('num'))
+
+res3.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# schema = StructType([
+#     StructField("person_id", IntegerType(), True),
+#     StructField("person_name", StringType(), True),
+#     StructField("weight", IntegerType(), True),
+#     StructField("turn", IntegerType(), True)
+# ])
+
+# data = [
+#     (5, "Alice", 250, 1),
+#     (4, "Bob", 175, 5),
+#     (3, "Alex", 350, 2),
+#     (6, "John Cena", 400, 3),
+#     (1, "Winston", 500, 6),
+#     (2, "Marie", 200, 4)
+# ]
+
+
+# df = spark.createDataFrame(data, schema)
+
+
+# wind  = Window.orderBy(col('turn').asc())
+
+# df1 = df.withColumn('sum',sum('weight').over(wind))
+
+# df2 = df1.filter(col('sum') == 1000).select('person_name')
+
+# df2.show()
